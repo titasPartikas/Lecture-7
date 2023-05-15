b5a302fa840514271c5ca4f3a054a65153354a1f
titas.partikas <titas.partikas@devbridge.com>
2023-05-15 20:17:38 +0300

    init

---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const server = express();
+const mongoClient = new MongoClient(
+  "mongodb+srv://titas:TgxLVYkSAXc0iLQx@db.xoxi5ar.mongodb.net/"
+);
+server.use(express.json());
+server.use(cors());
+
+// const rentalCars = [];
+
+server.get("/rental-cars", async (_, res) => {
+  const mongoCluster = await mongoClient.connect();
+  const rentalCars = await mongoCluster
+    .db("Rental")
+    .collection("RentalCars")
+    .find()
+    .toArray();
+  mongoCluster.close();
+
+  res.send(rentalCars);
+});
+
+server.get("/rental-cars/:carBrand", (req, res) => {
+  const rentalCarsByCarBrand = rentalCars.filter((rentalCar) => {
+    return (
+      rentalCar.carBrand.toLowerCase() === req.params.carBrand.toLowerCase()
+    );
+  });
+  res.send(rentalCarsByCarBrand);
+});
+
+server.get("/car-brands", (_, res) => {
+  const carBrands = rentalCars.map((rentalCar) => {
+    return rentalCar.carBrand;
+  });
+  const uniqueCarBrands = [...new Set(carBrands)];
+
+  res.send(uniqueCarBrands);
+});
+
+server.post("/register-car", (req, res) => {
+  const payload = req.body;
+  const newRentalCar = {
+    owner: {
+      firstName: payload.firstName,
+      lastName: payload.lastName,
+      gender: payload.gender,
+    },
+    carBrand: payload.carBrand,
+  };
+
+  rentalCars.push(newRentalCar);
+
+  res.status(201).end();
+});
+
+server.listen(8080, () => console.log("server is running at 8080"));
